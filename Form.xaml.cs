6b3a5b52141355c1ed02e9666307f597560d9b8f
Akshay Deshmukh <ardehmukh45@gmail.com>
2022-04-07 12:54:15 +0530

    Add project files.

---
+++ Form.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfApp1.model;
+using WpfApp1.Service;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for Form.xaml
+    /// </summary>
+    public partial class Form : Window
+    {
+
+        UserDbService _userDbService;
+        
+
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        public void Start()
+        {
+            _userDbService = new UserDbService();
+            DataContext = _userDbService;
+            
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Start();
+        }
+
+        private void ButtonInsert_Click(object sender, RoutedEventArgs e)
+        {
+            _userDbService.Insert();
+            TextBoxUsername.Focus();
+        }
+
+        private void ButtonUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            _userDbService.Update();
+        }
+
+        private void ButtonFirst_Click(object sender, RoutedEventArgs e)
+        {
+            _userDbService.First();
+        }
+
+        private void ButtonPrevious_Click(object sender, RoutedEventArgs e)
+        {
+            _userDbService.Previous();
+        }
+
+        private void ButtonNext_Click(object sender, RoutedEventArgs e)
+        {
+            _userDbService.Next();
+        }
+
+        private void ButtonLast_Click(object sender, RoutedEventArgs e)
+        {
+            _userDbService.Last();
+        }
+
+        private void ButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+            _userDbService.Save();
+        }
+
+        private void ButtonCancel_Click(object sender, RoutedEventArgs e)
+        {
+            _userDbService.StopEditing();
+        }
+    }
+}
---
+++ Service/UserDbService.cs
@@ -0,0 +1,231 @@
+﻿using Microsoft.Data.SqlClient;
+using System.ComponentModel;
+using System.Data;
+
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using WpfApp1.model;
+
+namespace WpfApp1.Service
+{
+    internal class UserDbService : INotifyPropertyChanged
+    {
+        public User User { get; set; }
+
+        public int RecordNumber { get; private set; }
+        public int TotalRecords { get; private set; }
+
+        public UserDbService()
+        {
+            LoadInitialView();
+        }
+
+        private void LoadInitialView()
+        {
+            RecordNumber = 1;
+            TotalRecords = GetTotalRecordFromDb();
+
+            SelectFromDb();
+        }
+
+        public void Insert()
+        {
+            RecordMode = RecordMode.Insert;
+            User = new User();
+
+            NotifyPersonRecordChanged();
+        }
+
+        public void Update()
+        {
+            RecordMode = RecordMode.Update;
+        }
+
+        public void Save()
+        {
+            if (RecordMode == RecordMode.Insert)
+            {
+                InsertIntoDb();
+                TotalRecords++;
+                RecordNumber = TotalRecords;
+            }
+            else if (RecordMode == RecordMode.Update)
+                UpdateInDb();
+            else
+                return;
+
+            StopEditing();
+        }
+
+        public void StopEditing()
+        {
+            if (RecordMode == RecordMode.Insert)
+                SelectFromDb();
+
+            RecordMode = RecordMode.View;
+        }
+
+        private void UpdateInDb()
+        {
+            var query = $"UpdatePerson";
+            using var connection = new SqlConnection(Properties.Settings.Default.ConnectionString);
+            using var command = new SqlCommand(query, connection);
+            command.CommandType = CommandType.StoredProcedure;
+
+            command.Parameters.AddWithValue("@Username", User.Username);
+            command.Parameters.AddWithValue("@Password", User.Password);
+            command.Parameters.AddWithValue("@Age", User.Age);
+            command.Parameters.AddWithValue("@Email", User.Email);
+
+            connection.Open();
+
+            command.ExecuteNonQuery();
+        }
+
+        private void InsertIntoDb()
+        {
+            var query = "InsertUser";
+
+            using var connection = new SqlConnection(Properties.Settings.Default.ConnectionString);
+            using var command = new SqlCommand(query, connection);
+
+            command.CommandType = CommandType.StoredProcedure;
+
+            connection.Open();
+
+            command.Parameters.AddWithValue("@Username", User.Username);
+            command.Parameters.AddWithValue("@Password", User.Password);
+            command.Parameters.AddWithValue("@Age", User.Age);
+            command.Parameters.AddWithValue("@Email", User.Email);
+
+            command.ExecuteNonQuery();
+        }
+
+        private static int GetTotalRecordFromDb()
+        {
+            var query = $"select count(Username) from Form";
+            using var connection = new SqlConnection(Properties.Settings.Default.ConnectionString);
+            using var command = new SqlCommand(query, connection);
+
+            connection.Open();
+
+            using var reader = command.ExecuteReader();
+
+            reader.Read();
+
+            var recordCount = (int)reader[0];
+            return recordCount;
+        }
+
+        public void SelectFromDb()
+        {
+            using var connection = new SqlConnection(Properties.Settings.Default.ConnectionString);
+            using var command = new SqlCommand("SelectUser", connection);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@RecordNumber", RecordNumber);
+
+            connection.Open();
+
+            using var reader = command.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                reader.Read();
+
+                User = new User
+                {
+                    Username = reader["Username"].ToString(),
+                    Password = reader["Password"].ToString(),
+                    Age = (int)reader["Age"],
+                    Email = reader["Email"].ToString()
+                };
+            }
+            else
+                User = new User();
+
+            NotifyPersonRecordChanged();
+        }
+
+        private void NotifyPersonRecordChanged()
+        {
+            OnPropertyChanged(nameof(User));
+            OnPropertyChanged(nameof(RecordNumber));
+            OnPropertyChanged(nameof(TotalRecords));
+        }
+
+        public void First()
+        {
+            if (RecordNumber != 1)
+            {
+                RecordNumber = 1;
+                SelectFromDb();
+            }
+        }
+
+        public void Previous()
+        {
+            if (RecordNumber > 1)
+            {
+                RecordNumber--;
+                SelectFromDb();
+            }
+        }
+
+        public void Next()
+        {
+            if (RecordNumber < TotalRecords)
+            {
+                RecordNumber++;
+                SelectFromDb();
+            }
+        }
+
+        public void Last()
+        {
+            if (RecordNumber != TotalRecords)
+            {
+                RecordNumber = TotalRecords;
+                SelectFromDb();
+            }
+        }
+
+        #region RecordViewState
+
+        private RecordMode _recordMode = RecordMode.View;
+        public RecordMode RecordMode
+        {
+            get
+            {
+                return _recordMode;
+            }
+            set
+            {
+                _recordMode = value;
+                OnPropertyChanged(nameof(InputIsReadOnly));
+                OnPropertyChanged(nameof(InputIsEditable));
+            }
+        }
+
+        public bool InputIsReadOnly
+        {
+            get
+            {
+                return RecordMode != RecordMode.Insert && RecordMode != RecordMode.Update;
+            }
+        }
+
+        public bool InputIsEditable => !InputIsReadOnly;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // Create the OnPropertyChanged method to raise the event
+        // The calling member's name will be used as the parameter.
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        #endregion
+
+    }
+}
